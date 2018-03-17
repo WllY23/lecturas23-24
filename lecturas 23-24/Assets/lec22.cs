023277f046b2de4e76e7dbbaae2d570aa175be36
WllY23 <arias1533@gmail.com>
2018-03-17 12:21:29 -0500

    lecturas 23-24
    monto de las lecturas 23 & 24

---
+++ lecturas 23-24/Assets/lec22.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lec22 : MonoBehaviour 
+{
+    // definicion de las variables 
+	public float a;
+	string saludo;
+	int num1=2,num2=2,num3=1,num4=5,num5=10,num6=15,num7=20,num8=32,num9=12,num10=1;
+	public float s;
+	float p1=5f,p2=8f,p3=6f,p4=10f,p5=15f,sumval,pro;
+	float n1=13.8f,n2=23.7f,n3=15.4f,n4=1.1f,n5=7.8f,n6=90.6f,n7=103.9f,n8=11.7f,n9=22.9f,n10=100.1f;
+	public float sum2c;
+	public float promm;
+	void Start () 
+	{
+		// 1- imprimir hola mundo
+		saludo = ("hola_mundo");   // saludo es un string que me imprimira el hola mundo
+        Debug.Log ("punto 1:"); Debug.Log (saludo);  // deubg.log es la manera de inprimir en c# 
+		// 2-suma el valor de 2 numeros 
+		a= (5f + 8f);  // a es la variable que toma el valor que se le fue asignado que en este caso es 5+8
+        Debug.Log ("punto 2:"); Debug.Log (a);
+		// 3- sumar 10 variables
+		s = (num1+num2+num3+num4+num5+num6+num7+num8+num9+num10); 
+		Debug.Log ("punto 3:"); Debug.Log (s);
+		// 4- imprime el promedio de 5 literales
+		sumval = (p1+p2+p3+p4+p5);     // suma de los valores 
+        pro = (sumval / 5);            // pro es la suma de los valores de sumval y dividirlos por 5
+		Debug.Log ("punto 4:"); Debug.Log (pro);
+		// 5- imprime prmedio de 12 variables decimales flota o double
+		sum2c = (n1+n2+n3+n4+n5+n6+n7+n8+n9+n10); // suman de valores 
+		promm = (sum2c / 10);                     // promedio de la suma dividido  10
+		Debug.Log ("punto 5:"); Debug.Log (promm);
+	}
+	void Update () {}
+}
---
+++ lecturas 23-24/Assets/lec23.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lec23 : MonoBehaviour
+{
+    public bool crear;
+    public bool crearnegras;
+    public bool dest;
+    int coller = 1;
+    public GameObject bolas;
+    // cal y cal2 son para el random de los for
+    public int cal;
+    public int cal2;
+    public GameObject[,] mat;
+    void Start(){}
+    void Creacion()  // este es el metodo que me crea la matriz con el game objet bolas
+    {
+        /*este es el ramdom que me controla las filas y las columnas
+        el para dentro de otro para para crear las filas y las columnas que se generaran con el objeto vacio.*/
+        cal = (Random.Range(3, 12));
+        cal2 = (Random.Range(3, 12));
+        // esta es la matriz que sera llenada por cal y cal2 
+        mat = new GameObject[cal,cal2];
+        // estos son los 2 paras que me llenaran la matriz 
+        for (int i = 0; i < cal; i++)
+        {
+            for (int j = 0; j < cal2; j++)
+            {
+                // esta parte es la que me crea las esferas y la posicion en la que se va a ubicar. 
+                GameObject bolas = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                bolas.transform.position = new Vector3(i * 1.2f, j * 1.2f, 0f);
+                mat[i,j] = bolas;
+                // bolas.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+                // color = new Color(Random.value, Random.value, Random.value, 1.0f);
+                // este es un ramdom para el caso de los colores  
+                coller = Random.Range(1, 4);
+                // este es un caso que le da los colores del gameobjet 
+                switch (coller)
+                {
+                    case 1:
+                        bolas.GetComponent<MeshRenderer>().material.color = Color.red;
+                        break;
+                    case 2:
+                        bolas.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                        break;
+                    case 3:
+                        bolas.GetComponent<MeshRenderer>().material.color = Color.green;
+                        break;
+                    case 4:
+                        bolas.GetComponent<MeshRenderer>().material.color = Color.blue;
+                        break;
+                }
+            }
+        }
+    }
+    void Update()
+    {
+    	// esta es una condicion boleana que al ser verdadera me creara la matriz
+        if (crear == true)
+        {
+            crear = false;
+            Creacion();
+        }
+        // esta es una condicion boleana que me comparara los colores y los pone negros 
+        if (crearnegras == true)
+        {
+            crearnegras = false;
+            comp();
+            
+        }
+        // este es una condicion boleana que al ser verdadera me destruye la matriz 
+        if (dest == true)
+        {
+            dest = false;
+            destruir();
+            
+        }
+    }
+    // este es un metodo que me compara todas las posiciones de la matriz 
+    void comp() // este es el metrodo que me compara las posociones de la matriz 
+    {
+        try
+        { 
+            for (int i = 0; i < cal ; i++)
+            {
+                for (int j = 0; j < cal2 ; j++)
+                {
+                    if (i > 0)
+                    {
+                        if (mat[i,j].GetComponent<MeshRenderer>().material.color == mat[i-1,j].GetComponent<MeshRenderer>().material.color)
+                        {
+                            mat[i,j].GetComponent<MeshRenderer>().material.color = Color.black;
+                            mat[i-1,j].GetComponent<MeshRenderer>().material.color = Color.black;
+                        }
+                    }
+                }
+            }
+        } catch { }
+
+    }
+    // este es el metodo que me destruye la matriz recorriendola y  destruyendola
+    void destruir()
+    {
+        for (int i = 0; i < cal; i++)
+        {
+            for (int j = 0; j < cal2; j++)
+            {
+                Destroy(mat[i,j]);
+            }
+        }
+    }
+}
+ 
